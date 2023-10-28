d1c3e8ab0a3ca4f9a625bab652fa2ccdbc2a264f
evanpurl <evan@purlcomputers.com>
2023-10-27 22:20:29 -0600

    Updated requirements.txt, fixed bugs in tickets and added auto close button.

--- cogs/Tickets.py
+++ cogs/Tickets.py
@@ -4,13 +4,16 @@ import chat_exporter
 import discord
 from discord import app_commands, ui
 from discord.ext import commands
-import datetime
+from datetime import datetime, timedelta
+from pytz import timezone
+from time import mktime
 
 
-def ticketmessageembed():
-    embed = discord.Embed(title="**Tickets**",
-                          description=f"Change me",
-                          color=discord.Color.orange())
+def ticketmessageembed(server):
+    embed = discord.Embed(title="ServerOwner | Support Tickets!",
+                          description=f"ServerOwner, your ultimate Discord Hub for Minecraft server enthusiasts, is delighted to have you join our support team. Whether you're new to the community, a dedicated agent, a talented person, or a unique person, you've found your new home for being an agent. We offer a variety of things tailored to your expertise and interests.\n\n**Notice:** Repeatedly alerting staff in non-emergency tickets may lead to a ban on creating new tickets. We kindly ask for your patience; our team reviews tickets every few hours.",
+                          color=discord.Color.orange(), timestamp=datetime.now())
+    embed.set_thumbnail(url=server.icon.url)
     embed.set_footer(text="©️ ServerOwner 2023")
     return embed
 
@@ -18,8 +21,27 @@ def ticketmessageembed():
 async def ticketembed(user, reason, server):
     embed = discord.Embed(title="ServerOwner | New Ticket!",
                           description=f"""Hello, {user.mention}. Thank you for reaching out to our Support Team! We'll be with you shortly. In the interim, please provide a detailed description of your concerns.""",
-                          color=discord.Color.orange(), timestamp=datetime.datetime.now())
+                          color=discord.Color.orange(), timestamp=datetime.now())
     embed.add_field(name=f"Reason:", value=reason, inline=True)
+    embed.set_thumbnail(url=server.icon.url)
+    embed.set_footer(text="©️ ServerOwner 2023")
+    return embed
+
+
+async def ticketautocloseembed(server):
+    async def timetounix():
+        mst = timezone('EST')  # no reason to configure, discord automatically sets to your timezone using Relative time.
+
+        time_to_run = datetime.now(mst) + timedelta(hours=48)
+
+        return f'<t:{int(mktime(time_to_run.timetuple()))}:R>'
+
+    embed = discord.Embed(title="ServerOwner | Ticket Closure!",
+                          description=f"""This ticket will be closed in {await timetounix()} due to inactivity.""",
+                          color=discord.Color.orange(), timestamp=datetime.now())
+    embed.add_field(name=f"Info:",
+                    value=f"If you need further assistance, please reply to this message. Otherwise, you can close this ticket using the button in the panel above or simply ignore this message.",
+                    inline=True)
     embed.set_thumbnail(url=server.icon.url)
     embed.set_footer(text="©️ ServerOwner 2023")
     return embed
@@ -30,7 +52,7 @@ class ticketbutton(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Create Ticket", emoji="📨", style=discord.ButtonStyle.blurple,
+    @discord.ui.button(label="Create Ticket", emoji="📩", style=discord.ButtonStyle.blurple,
                        custom_id="ticketbutton")
     async def gray_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
@@ -131,6 +153,55 @@ class ticketbuttonpanel(discord.ui.View):
         except Exception as e:
             print(e)
 
+    @commands.has_permissions(manage_channels=True)
+    @discord.ui.button(label="Auto-Close Ticket", emoji="⏲️", style=discord.ButtonStyle.gray,
+                       custom_id="ticket:autoclose")
+    async def auto_close_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        try:
+            if interaction.user.guild_permissions.manage_channels:
+                await interaction.response.send_message(content="Timer started.", ephemeral=True)
+                await interaction.channel.send(embed=await ticketautocloseembed(interaction.guild))
+
+                def check(m: discord.Message):  # m = discord.Message.
+                    return m.author.id == interaction.user.id and m.channel.id == interaction.channel.id
+
+                try:
+                    msg = await interaction.client.wait_for('message', check=check,
+                                                            timeout=172800)  # 48 hours in seconds.
+                    await interaction.channel.send(f"Timer cancelled.")
+                except asyncio.TimeoutError:  # runs from here if the above timeout ends.
+                    logchannel = discord.utils.get(interaction.guild.channels,
+                                                   id=1155617191828410418)
+                    if logchannel:
+                        transcriptdm = await chat_exporter.export(
+                            interaction.channel,
+                        )
+                        transcripttochannel = await chat_exporter.export(
+                            interaction.channel,
+                        )
+                        if transcriptdm is None:
+                            return
+                        if transcripttochannel is None:
+                            return
+
+                        transcript_file_to_dm = discord.File(
+                            io.BytesIO(transcriptdm.encode()),
+                            filename=f"transcript-{interaction.channel.name}.html",
+                        )
+                        transcript_file_to_channel = discord.File(
+                            io.BytesIO(transcripttochannel.encode()),
+                            filename=f"transcript-{interaction.channel.name}.html",
+                        )
+
+                        await interaction.user.send(file=transcript_file_to_dm)
+                        await logchannel.send(file=transcript_file_to_channel)
+
+                    await interaction.channel.delete()
+            else:
+                await interaction.response.send_message(content="You don't have permission to do that.", ephemeral=True)
+        except Exception as e:
+            print(e)
+
 
 class ticketcmd(commands.Cog):
     def __init__(self, bot):
@@ -140,8 +211,9 @@ class ticketcmd(commands.Cog):
     @app_commands.command(name="ticket", description="Command used to make a ticket.")
     async def ticket(self, interaction: discord.Interaction, channel: discord.TextChannel) -> None:
         try:
-            await channel.send(embed=ticketmessageembed(), view=ticketbutton())
-            await interaction.response.send_message(content=f"Ticket message created in {channel.mention}", ephemeral=True)
+            await channel.send(embed=ticketmessageembed(interaction.guild), view=ticketbutton())
+            await interaction.response.send_message(content=f"Ticket message created in {channel.mention}",
+                                                    ephemeral=True)
         except Exception as e:
             print(e)
 
@@ -158,9 +230,11 @@ class ticketcmd(commands.Cog):
                     await interaction.response.send_message(content=f"Added {user.name} to the ticket.",
                                                             ephemeral=True)
                 else:
-                    await interaction.response.send_message(content=f"You are not currently within a ticket.", ephemeral=True)
+                    await interaction.response.send_message(content=f"You are not currently within a ticket.",
+                                                            ephemeral=True)
             else:
-                await interaction.response.send_message(content=f"Your access does not authorize the use of this command.", ephemeral=True)
+                await interaction.response.send_message(
+                    content=f"Your access does not authorize the use of this command.", ephemeral=True)
         except Exception as e:
             print(e)
 
@@ -174,11 +248,13 @@ class ticketcmd(commands.Cog):
                     overwrite.send_messages = False
                     overwrite.read_messages = False
                     await interaction.channel.set_permissions(user, overwrite=overwrite)
-                    await interaction.response.send_message(content=f"Removed {user.name} from the ticket.", ephemeral=True)
+                    await interaction.response.send_message(content=f"Removed {user.name} from the ticket.",
+                                                            ephemeral=True)
                 else:
                     await interaction.response.send_message(content=f"You aren't in a ticket.", ephemeral=True)
             else:
-                await interaction.response.send_message(content=f"Your access does not authorize the use of this command.", ephemeral=True)
+                await interaction.response.send_message(
+                    content=f"Your access does not authorize the use of this command.", ephemeral=True)
         except Exception as e:
             print(e)
 
